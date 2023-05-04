a0519e8eccb05d6aee8646d196baa60755351655
Jean Nanchen <jean.nanchen@hevs.ch>
2023-05-04 13:30:17 +0200

    add test-thread.cpp

---
+++ tspcc/test-thread.cpp
@@ -0,0 +1,17 @@
+// Test thread
+// -----------------------------------------------------------------------
+
+#include <iostream>
+#include <thread>
+
+void thread_func(int id)
+{
+    std::cout << "Hello, thread! id :" << id << "\n";
+}
+
+int main(int argc, char* argv[])
+{
+    std::thread t(thread_func, 1);
+    t.join();
+    return 0;
+}
